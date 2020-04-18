c9c435dead084ea0ba88a6859e7014485a6c38c7
100sun <sunhyebaek99@gmail.com>
2020-04-18 23:55:03 +0900

    level2

---
+++ hw1/json_level2,3.js
@@ -0,0 +1,11 @@
+var sprite7 = [
+    { name : '박현주', age : '25', univ : '단국대'},
+    { name : '김성윤', age : '23', univ : '중앙대'},
+    { name : '백선혜', age : '22', univ : '성신여대'},
+    { name : '천명희', age : '24', univ : '덕성여대'},
+    { name : '최정균', age : '25', univ : '한서대'}
+];
+
+sprite7.forEach( 
+    sprite7 => console.log(sprite7.name+'씨는 나이가 ' + sprite7.age + '이고, ' + sprite7.univ + '학교를 다닙니다.')
+    );
---
+++ hw1/json_sprite7.js
@@ -0,0 +1,12 @@
+
+var sprite7 = [
+    { name : '박현주', age : '25', univ : '단국대'},
+    { name : '김성윤', age : '23', univ : '중앙대'},
+    { name : '백선혜', age : '23', univ : '성신여대'},
+    { name : '천명희', age : '23', univ : '덕성여대'},
+    { name : '최정균', age : '23', univ : '한서대'}
+];
+
+sprite7.forEach( 
+    sprite7 => console.log(sprite7.name+'이는 종이 '+dog.family+'이고, 나이가 '+dog.age+'세입니다 ~')
+    );
